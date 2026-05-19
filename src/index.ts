b3d7f3003926a4a6e1b377f72ef3adb36847c6e6
LaDoodleInTheHat <120087497+LaDoodleInTheHat@users.noreply.github.com>
2026-05-19 01:22:49 +0000

    add main menu

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,39 @@ import {
   createCliRenderer,
   Text,
   TextAttributes,
+  SelectRenderable
 } from "@opentui/core";
 
 const renderer = await createCliRenderer({ exitOnCtrlC: true });
 
-renderer.root.add(
-  Box(
-    { alignItems: "center", justifyContent: "center", flexGrow: 1 },
-    Box(
-      { justifyContent: "center", alignItems: "flex-end" },
-      ASCIIFont({ font: "tiny", text: "OpenTUI" }),
-      Text({ content: "What will you build?", attributes: TextAttributes.DIM }),
-    ),
-  ),
-);
+class Game {
+  start() {
+    const selector = new SelectRenderable(renderer, {
+      id: "start-menu",
+      width: 25,
+      height: 10,
+      itemSpacing: 1,
+      backgroundColor: "transparent",
+      focusedBackgroundColor: "transparent",
+      options: [
+        { name: "New Game", description: "Start a new save" },
+        { name: "Load Game", description: "Load an existing save" },
+        { name: "Quit", description: "Exit the game" }
+      ]
+    });
+
+    renderer.root.add(
+      Box({alignItems: "center", justifyContent: "center", width: "100%", height: "100%"},
+        Box({alignItems: "center", justifyContent: "center"},
+          ASCIIFont({font: "slick", text: "Doodle Tale"})
+        ),
+        Box({alignItems: "center", justifyContent: "center", paddingTop: 2},
+          selector
+        )
+      )
+    )
+  }
+}
+
+var game = new Game();
+game.start();
